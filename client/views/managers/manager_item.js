98092b4ac06d14f91eb1fffc1ddbe9d2c070ab4a
Jonathan Katz <jonathan.a.katz@gmail.com>
2015-01-08 16:40:38 -0800

    added a managers collection

---
+++ client/views/managers/manager_item.js
@@ -0,0 +1,3 @@
+Template.managerItem.helpers({
+
+});
--- client/views/managers/managers_list.js
+++ client/views/managers/managers_list.js
@@ -2,16 +2,19 @@ var managersData = [
   {
     name: 'Jonathan',
     teamName: 'FlyerMaker',
+    managerPage: 'http://hockey.fantasysports.yahoo.com/hockey/26488/3',
     wins: '10'
   },
   {
     name: 'Ben',
     teamName: 'FreshPrinceOfBriere',
+    managerPage: 'http://hockey.fantasysports.yahoo.com/hockey/26488/1',
     wins: '9'
   },
   {
     name: 'MattyB',
     teamName: 'Icebirds',
+    managerPage: 'http://hockey.fantasysports.yahoo.com/hockey/26488/5',
     wins: '3'
   }
 ];
