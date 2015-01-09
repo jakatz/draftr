dcd544a78cddee37bc3eccaa7255206f2fee42ab
Jonathan Katz <jonathan.a.katz@gmail.com>
2015-01-08 16:56:28 -0800

    wired collection into managersList template

--- client/views/managers/managers_list.js
+++ client/views/managers/managers_list.js
@@ -1,24 +1,5 @@
-var managersData = [
-  {
-    name: 'Jonathan',
-    teamName: 'FlyerMaker',
-    managerPage: 'http://hockey.fantasysports.yahoo.com/hockey/26488/3',
-    wins: '10'
-  },
-  {
-    name: 'Ben',
-    teamName: 'FreshPrinceOfBriere',
-    managerPage: 'http://hockey.fantasysports.yahoo.com/hockey/26488/1',
-    wins: '9'
-  },
-  {
-    name: 'MattyB',
-    teamName: 'Icebirds',
-    managerPage: 'http://hockey.fantasysports.yahoo.com/hockey/26488/5',
-    wins: '3'
-  }
-];
-
 Template.managersList.helpers({
-  managers: managersData
+  managers: function() {
+    return Managers.find();
+  }
 });
